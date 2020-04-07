d1cdf9f88707360b4826ff10505b5f786c0c3bd3
itagaev <tagayev.ilias@gmail.com>
2020-04-07 15:22:10 +0600

    Updates

---
+++ precache-manifest.9921609719855651ec7af01ca41fd5dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22815c8930a93307561a300bbce5d3ba",
+    "url": "/HotelRent/index.html"
+  },
+  {
+    "revision": "966ef893025c516d229a",
+    "url": "/HotelRent/static/js/2.66b83522.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/HotelRent/static/js/2.66b83522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d8b58b04715d0ac234",
+    "url": "/HotelRent/static/js/main.9ebf58d9.chunk.js"
+  },
+  {
+    "revision": "dd0a3a3d5af7c7d0edc5",
+    "url": "/HotelRent/static/js/runtime-main.ba638760.js"
+  }
+]);
